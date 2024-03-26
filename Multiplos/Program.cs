32c2603b259b0cf068c9bf83fd075cbae81ea4c7
Usuario <Usuario@CPD03-M03>
2024-03-26 10:48:38 -0300

    Adicionar arquivos de projeto.

---
+++ Multiplos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+
+            Console.WriteLine("Digite um número: ");
+            double num = double.Parse(Console.ReadLine());
+
+            while (cont <= 100)
+            {
+                double mult = num * cont;
+                Console.WriteLine(mult);
+                cont++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
